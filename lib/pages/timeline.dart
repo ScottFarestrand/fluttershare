db8f996f8844ef2e89f27a1857b7fc3988e19fb5
ScottFarestrand <ScottFarestrand@gmail.com>
2021-01-21 05:29:16 -0800

    End of Lesson 14 Added getusers toTime line (Mostly for Demo)

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,5 +1,10 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 import '../widgets/header.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final CollectionReference usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -7,6 +12,23 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    print("Getting Users");
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() {
+    print("getting docs");
+    usersRef.getDocuments().then((QuerySnapshot snapshot) {
+      print("printing error");
+      snapshot.documents.forEach((DocumentSnapshot doc) {
+        print(doc.data);
+      });
+    });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
