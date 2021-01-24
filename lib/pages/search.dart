11841fa2fef10049751251fc20e462a436375cb5
ScottFarestrand <ScottFarestrand@gmail.com>
2021-01-23 20:00:46 -0800

    End of Lesson 25 added responsive sizing

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,9 +8,60 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  AppBar buildSearchScreen() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        decoration: InputDecoration(
+          hintText: "Search for a User",
+          filled: true,
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 28,
+          ),
+          suffix: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => print("Cleared"),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildNoContent() {
+    final orientaton = MediaQuery.of(context).orientation;
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/images/search.svg',
+              // height: 300,
+              height: orientaton == Orientation.portrait ? 300.0 : 200.0,
+            ),
+            Text(
+              "Find Users",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 60.0,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+        appBar: buildSearchScreen(),
+        body: buildNoContent());
   }
 }
 
